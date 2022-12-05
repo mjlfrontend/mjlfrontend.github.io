6f9a972135ad0472fa98dc7d7d518af565aa3eba
markjaysonlacandula <markjaysonlacandula@gmail.com>
2022-12-05 14:01:29 +0800

    fix folder structure

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   mode: "jit",
-  content: ["*.{html,js}"],
+  content: ["public/*.{html,js}"],
   theme: {
     extend: {},
     container: {
