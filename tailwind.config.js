690fa5ea4b4ee203e78d1f4fb5d62d328cdf39eb
markjaysonlacandula <markjaysonlacandula@gmail.com>
2022-12-05 12:51:14 +0800

    test upload

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: "jit",
+  content: ["public/*.{html,js}"],
+  theme: {
+    extend: {},
+    container: {
+      center: true,
+    },
+  },
+  plugins: [],
+};
